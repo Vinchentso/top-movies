27a256b0ee7c1917681ee1680e97d4e47e0662b6
Vinchentso <ventsi_gt@abv.bg>
2021-12-10 17:36:04 +0200

    Add files via upload

---
+++ build/precache-manifest.ce571d4a58cc980abacd0dfddd974b3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  },
+  {
+    "revision": "80e9d8a2ff7fddf4a6dc1309d9e8a3ff",
+    "url": "/static/media/search-icon.80e9d8a2.svg"
+  },
+  {
+    "revision": "5de5919a93cba46a09c8df1296a094ee",
+    "url": "/static/media/react-movie-logo.5de5919a.svg"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bba9d8b15712eb636073",
+    "url": "/static/js/main.bba9d8b1.chunk.js"
+  },
+  {
+    "revision": "3f1e0b3921868eff4435",
+    "url": "/static/js/1.3f1e0b39.chunk.js"
+  },
+  {
+    "revision": "24f05974f3e066024cf44e88fbd22a9a",
+    "url": "/index.html"
+  }
+];
