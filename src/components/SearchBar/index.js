af5b8b0f91d80eab100bd374c4af1869ca5d8813
Lyudmil <lyudmilvst@gmail.com>
2022-01-05 14:51:05 +0000

    New addition

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
+//DALI SE CHETE TOVA
 
 //image
 import searchIcon from '../../images/search-icon.svg';
